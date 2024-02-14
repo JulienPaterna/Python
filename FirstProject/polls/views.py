2519954a309af821658ca6fe73e9b0bc41f861a3
JulienPaterna <paterna.julien@gmail.com>
2024-02-14 14:15:54 +0100

    [USE] Gabarits/test coucou.html

--- FirstProject/polls/views.py
+++ FirstProject/polls/views.py
@@ -1,19 +1,13 @@
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.template import loader
 from .models import Question
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("T'ES CON OU QUOI???")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
@@ -29,3 +23,7 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+def coucou(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/coucou.html", {"question": question})
