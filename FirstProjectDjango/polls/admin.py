252c94dbdf8b392fe14adbd8533b0e637881f138
JulienPaterna <paterna.julien@gmail.com>
2024-02-21 12:08:36 +0100

    [ADD] fichiers statiques + gabarits de base

--- FirstProjectDjango/polls/admin.py
+++ FirstProjectDjango/polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Date information", {"fields": ["pub_date"]}),
+        ("Détail", {"fields": ["question_text"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
